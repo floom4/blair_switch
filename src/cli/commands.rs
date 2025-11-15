fe3778b3d4bbe921453a80172c075c539dd317c9
corentin <lebigot.corentin@gmail.com>
2025-11-15 02:36:33 +0100

    refacto(cli): Move cli handling
    - Move commands handling into command.rs
    - Centralized commands pattern, description and handling inside
      one file with authority over what commands are available

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,120 +1,256 @@
-use super::shell::CliMode;
+use std::collections::{HashMap, HashSet};
+use std::process;
+use std::sync::Arc;
 
-struct Command<'a> {
-  pattern: &'a [&'a str] ,
+use crate::fib::Fib;
+use crate::network::interface::{InterfaceView, IntfCmd};
+use super::shell::{CliMode, IntfsViewMap};
+
+pub struct Command<'a> {
+  pub pattern: &'a [&'a str] ,
   description: &'a str,
-  //handler: fn 
+  handler: fn(&IntfsViewMap, &Arc<Fib>, &mut CliMode, Arc<InterfaceView>, &mut HashMap<CliMode, HashSet<String>>, HashMap<String, String>),
 }
 
-const GENERAL_COMMANDS: &[Command] = &[
+pub const GENERAL_COMMANDS: &[Command] = &[
   Command {
     pattern: &["show", "interfaces"],
     description: "Show all interfaces information",
+    handler: | intfs_view, _, _, _, _, _ | {
+      let mut keys: Vec<_> = intfs_view.keys().cloned().collect();
+      keys.sort();
+      println!("Interfaces:\n==========\n");
+      for intf in keys {
+        println!("{}\n", intfs_view[intf]);
+      }
+    }
   },
   Command {
     pattern: &["show", "fib"],
     description: "Display FIB entries",
+    handler: | _, fib, _, _, _, _ | {
+      println!("FIB:\n====\n{}", fib)
+    }
   },
   Command {
     pattern: &["interface", "<intf>"],
     description: "Enter in interfate configuration mode for given target",
-  },
-  Command {
-    pattern: &["debug"],
-    description: "Set entire device in debug mode",
+    handler: | intfs_view, _, mode, _, _, args | {
+      let intf_name = &args["intf"];
+      if intfs_view.contains_key(&intf_name[..]) {
+        *mode = CliMode::Interface(intf_name.to_string());
+      } else {
+        println!("Interface {} not found", intf_name);
+      }
+    },
   },
   Command {
     pattern: &["debug"],
     description: "Set entire device and all interfaces in debug mode",
+    handler: | intfs_view, _, _, _, _, _ | {
+      for (_, view) in intfs_view {
+        view.set_debug_mode(true);
+      }
+    }
   },
   Command {
     pattern: &["no", "debug"],
     description: "Disable debug mode for entire device and interfaces",
+    handler: | intfs_view, _, _, _, _, _ | {
+      for (_, view) in intfs_view {
+        view.set_debug_mode(false);
+      }
+    }
   },
   Command {
     pattern: &["counters", "reset"],
     description: "Reset all counters on the device",
+    handler: | intfs_view, _, _, _, _, _ | {
+      for (_, view) in intfs_view {
+        view.reset_counters();
+      }
+    }
   },
   Command {
     pattern: &["show", "config"],
     description: "Display current running configuration",
+    handler: | _, _, _, _, config, _ | {
+      //TODO
+      //print!("{}", config_to_str(config))
+    },
   },
   Command {
     pattern: &["config", "save", "<filename>"],
     description: "Save current running configuration at <filename>",
+    handler: | _, _, _, _, config, args | {
+      //TODO
+      /*let filepath = &args["filename"]
+      if let Err(err) = fs::write(filepath, config_to_str(config)) {
+        eprintln!("Error: Failure to write config at \"{}\": {}", filepath, err);
+      } else {
+        println!("Config saved at {}", filepath);
+      }*/
+    },
   },
   Command {
     pattern: &["config", "load", "<filename>"],
     description: "Replace running configuration with config stored at <filename>",
+    handler: | _, _, _, _, _, _ | {
+      //TODO
+    }
   },
   Command {
     pattern: &["help"],
     description: "Display this help menu with available commandes",
+    handler: | _, _, _, _, _, _ | {
+      for cmd in GENERAL_COMMANDS {
+        println!("{:<40} {}", cmd.pattern.join(" "), cmd.description)
+      }
+    },
   },
   Command {
     pattern: &["exit"],
     description: "Exit and shutdown program",
+    handler: | _, _, _, _, _, _ | {
+      process::exit(0)
+    }
   },
 ];
 
-const INTF_COMMANDS: &[Command] = &[
+pub const INTF_COMMANDS: &[Command] = &[
   Command {
     pattern: &["show"],
     description: "Show interface information",
+    handler: | _, _, _, intf, _, _ | {
+      println!("{}", intf)
+    },
   },
   Command {
     pattern: &["debug"],
     description: "Enable debug mode on interface",
+    handler: | _, _, mode, curr_intf, config, _ | {
+      curr_intf.set_debug_mode(true);
+      //config.entry(mode.clone()).or_insert(HashSet::new()).insert(cmd.clone());
+    },
   },
   Command {
     pattern: &["no", "debug"],
     description: "Disable debug mode on interface",
+    handler: | _, _, _, intf, _, _ | {
+      intf.set_debug_mode(false)
+    },
   },
   Command {
     pattern: &["shutdown"],
     description: "Shut the interface off stopping ingress/egress traffic",
+    handler: | _, _, _, curr_intf, _, _ | {
+      curr_intf.send_cmd(IntfCmd::Shutdown);
+      //config.entry(mode.clone()).or_insert(HashSet::new()).insert(cmd.clone());
+    }
   },
   Command {
     pattern: &["no", "shutdown"],
     description: "Bring interface up re-establshing ingress/egress traffic",
+    handler: | _, _, _, intf, _, _ | {
+      intf.send_cmd(IntfCmd::NoShutdown)
+    },
   },
   Command {
     pattern: &["counters", "reset"],
     description: "Reset all interface counters to 0",
+    handler: | _, _, _, intf, _, _ | {
+      intf.reset_counters()
+    },
   },
   Command {
     pattern: &["switchport", "mode", "access"],
     description: "Set interface in vlan access mode",
+    handler: | _, _, _, intf, _, _ | {
+      intf.send_cmd(IntfCmd::PortModeAccess)
+    }
   },
   Command {
     pattern: &["switchport", "mode", "monitor", "<target_intf>"],
     description: "Set interface in monitor mode to mirror traffic from target interfaces",
+    handler: | intfs_view, _, _, intf, _, args | {
+      let target = &args["target_intf"];
+      if intfs_view.contains_key(&target[..]) {
+        intf.send_cmd(IntfCmd::PortModeMonitoring(target.to_string()));
+      } else {
+        println!("Interface {} not found", target);
+      }
+    }
   },
   Command {
     pattern: &["switchport", "access", "vlan", "<vlan>"],
     description: "Set vlan group for interface",
+    handler: | _, _, _, intf, _, args | {
+      let vlan_str = &args["vlan"];
+      match vlan_str.parse::<u16>() {
+        Ok(vlan) => {
+          if vlan > 0 && vlan < 4096 {
+            intf.send_cmd(IntfCmd::PortAccessVlan(vlan));
+            //config.entry(mode.clone()).or_insert(HashSet::new()).insert(cmd.clone());
+          } else {
+            eprintln!("Error: invalid vlan \"{}\". Must be an number between 1 and 4095", vlan_str);
+          }
+        },
+        Err(err) => eprintln!("Error: invalid vlan format \"{}\". Must be number between 1 and 4095", vlan_str),
+      }
+    },
   },
   Command {
     pattern: &["no", "switchport", "access", "vlan"],
     description: "Reset vlan group for interface to default group 1",
+    handler: | _, _, _, intf, _, _ | {
+      intf.send_cmd(IntfCmd::PortModeAccess)
+    },
   },
   Command {
     pattern: &["help"],
     description: "Display this help menu with available commandes",
+    handler: | _, _, _, _, _, _ | {
+      for cmd in INTF_COMMANDS {
+        println!("{:<40} {}", cmd.pattern.join(" "), cmd.description)
+      }
+    }
   },
   Command {
     pattern: &["exit"],
     description: "Exit interface configuration mode",
+    handler: | _, _, mode, _, _, _ | {
+      *mode = CliMode::General
+    }
   },
 ];
 
-pub fn display_help_menu(mode: &CliMode) {
-  let mut cmds =  match mode {
-    CliMode::General => GENERAL_COMMANDS,
-    CliMode::Interface(_) => INTF_COMMANDS,
-    _ => panic!(),
-  };
-  for cmd in cmds {
-    println!("{:<40} {}", cmd.pattern.join(" "), cmd.description)
+impl Command<'_> {
+  pub fn matches_pattern(&self, cmd: &String) -> bool {
+    let tokens : Vec<&str> = cmd.split(" ").collect();
+    if tokens.len() != self.pattern.len() {
+      return false;
+    }
+    for i in 0..tokens.len() {
+      if !(self.pattern[i].starts_with('<') && self.pattern[i].ends_with('>')) && tokens[i] != self.pattern[i] {
+        return false;
+      }
+    }
+    true
+  }
+
+  pub fn extract_args(&self, cmd: &String) -> HashMap<String, String> {
+    let mut result = HashMap::new();
+    let tokens : Vec<&str> = cmd.split(" ").collect();
+    for i in 0..self.pattern.len() {
+      if self.pattern[i].starts_with('<') && self.pattern[i].ends_with('>') {
+        result.insert(self.pattern[i].strip_prefix("<").unwrap().strip_suffix(">").unwrap().to_string(), tokens[i].to_string());
+      }
+    }
+    result
+  }
+
+  pub fn run(&self, intfs_view: &IntfsViewMap, fib: &Arc<Fib>, mode: &mut CliMode, intf: Arc<InterfaceView>, conf: &mut HashMap<CliMode, HashSet<String>>, cmd: &String ) {
+    let args = self.extract_args(cmd);
+    (self.handler)(intfs_view, fib, mode, intf, conf, args)
   }
 }
--- src/cli/shell.rs
+++ src/cli/shell.rs
@@ -9,6 +9,8 @@ use super::commands;
 use crate::network::interface::{InterfaceView, IntfCmd};
 use crate::fib::Fib;
 
+pub type IntfsViewMap<'a> = HashMap<&'a str, Arc<InterfaceView<'a>>>;
+
 #[derive(PartialOrd, Ord, Clone, Eq, PartialEq, Hash)]
 pub enum CliMode {
   General,
@@ -28,151 +30,53 @@ fn generate_prompt(mode: &CliMode) -> String {
   prompt
 }
 
-pub fn cli_run(intfs_view: &HashMap<&str, Arc<InterfaceView>>, fib: &Arc<Fib>,
+pub fn cli_run(intfs_view: &IntfsViewMap, fib: &Arc<Fib>,
   mirrors: &DashMap<String, Vec<Arc<InterfaceView>>> ) {
 
   let mut rl = rustyline::DefaultEditor::new().unwrap();
   let mut mode = CliMode::General;
   let mut config = HashMap::new();
 
-  loop {
+  'main: loop {
     let prompt = generate_prompt(&mode);
     let input = rl.readline(&prompt);
     match mode.clone() {
       CliMode::General => {
-        handle_general_cmd(&input, intfs_view, fib, &mut mode, &config);
+        match input {
+          Ok(ref cmd) => {
+            for available_cmd in commands::GENERAL_COMMANDS {
+              if available_cmd.matches_pattern(&cmd) {
+                available_cmd.run(intfs_view, fib, &mut mode, intfs_view.values().next().unwrap().clone(), &mut config, cmd);
+                continue 'main;
+              }
+            }
+            println!("Unknown command")
+          },
+          Err(ReadlineError::Interrupted) => println!("^C"),
+          Err(ReadlineError::Eof) => process::exit(0),
+          Err(_) => println!("No input"),
+        }
       },
       CliMode::Interface(ref if_name) => {
-        handle_interface_cmd(&input, intfs_view, fib, &mut mode, &mut config, if_name);
+        match input {
+          Ok(ref cmd) => {
+            for available_cmd in commands::INTF_COMMANDS {
+              if available_cmd.matches_pattern(&cmd) {
+                available_cmd.run(intfs_view, fib, &mut mode, intfs_view[&if_name[..]].clone(), &mut config, cmd);
+                continue 'main;
+              }
+            }
+            println!("Unknown command")
+          },
+          Err(ReadlineError::Interrupted) => println!("^C"),
+          Err(ReadlineError::Eof) => mode = CliMode::General,
+          Err(_) => println!("No input"),
+        }
       },
     }
     if let Ok(cmd) = input && let Err(err) = rl.add_history_entry(&cmd) {
       eprintln!("Err: {}", err);
     }
-  }
-}
-
-fn handle_general_cmd(input: &rustyline::Result<String>, intfs_view: &HashMap<&str, Arc<InterfaceView>>,
-  fib: &Arc<Fib>, mode: &mut CliMode, config: &HashMap<CliMode, HashSet<String>>) {
-
-  match input {
-    Ok(cmd) => {
-      /*if let Err(err) = rl.add_history_entry(&cmd) {
-        eprintln!("Err: {}", err);
-      }*/
-      let tokens : Vec<&str> = cmd.split(" ").collect();
-      match tokens.as_slice() {
-        ["show", "interfaces"] => {
-          let mut keys: Vec<_> = intfs_view.keys().cloned().collect();
-          keys.sort();
-          println!("Interfaces:\n==========\n");
-          for intf in keys {
-            println!("{}\n", intfs_view[intf]);
-          }
-        },
-        ["show", "fib"] => {
-          println!("FIB:\n====\n{}", fib)
-        }
-        ["interface", intf_name] => {
-            //if let Some(intf) = intfs_view.get(*intf_name) {
-            if intfs_view.contains_key(*intf_name) {
-              //mode = CliMode::Interface(intf.clone());
-              *mode = CliMode::Interface(intf_name.to_string());
-            } else {
-              println!("Interface {} not found", intf_name);
-            }
-        },
-        ["debug"] => {
-          for (_, view) in intfs_view {
-            view.set_debug_mode(true);
-          }
-        },
-        ["no", "debug"] => {
-          for (_, view) in intfs_view {
-            view.set_debug_mode(false);
-          }
-        },
-        ["counters", "reset"] => {
-          for (_, view) in intfs_view {
-            view.reset_counters();
-          }
-        }
-        ["show", "config"] => {
-          print!("{}", config_to_str(config));
-        }
-        ["config", "save", filepath] => {
-          if let Err(err) = fs::write(filepath, config_to_str(config)) {
-            eprintln!("Error: Failure to write config at \"{}\": {}", filepath, err);
-          } else {
-            println!("Config saved at {}", filepath);
-          }
-        },
-        ["config", "load", filepath] => {
-        },
-        ["help"] => commands::display_help_menu(&mode),
-        ["exit"] => process::exit(0),
-        _ => println!("Unknown command"),
-      }
-    },
-    Err(ReadlineError::Interrupted) => println!("^C"),
-    Err(ReadlineError::Eof) => process::exit(0),
-    Err(_) => println!("No input"),
-  }
-}
-
-fn handle_interface_cmd(input: &rustyline::Result<String>, intfs_view: &HashMap<&str, Arc<InterfaceView>>,
-  fib: &Arc<Fib>, mode: &mut CliMode, config: &mut HashMap<CliMode, HashSet<String>>, if_name: &String) {
-
-  let intf = &intfs_view[&if_name[..]];
-  match input {
-    Ok(cmd) => {
-      //if let Err(err) = rl.add_history_entry(&cmd) {
-       // eprintln!("Err: {}", err);
-      //}
-      let tokens : Vec<&str> = cmd.split(" ").collect();
-      match tokens.as_slice() {
-        ["show"] => println!("{}", intf),
-        ["debug"] => {
-          intf.set_debug_mode(true);
-          config.entry(mode.clone()).or_insert(HashSet::new()).insert(cmd.clone());
-        },
-        ["no", "debug"] => intf.set_debug_mode(false),
-        ["shutdown"] => {
-          intf.send_cmd(IntfCmd::Shutdown);
-          config.entry(mode.clone()).or_insert(HashSet::new()).insert(cmd.clone());
-        },
-        ["no", "shutdown"] => {intf.send_cmd(IntfCmd::NoShutdown);},
-        ["counters", "reset"] => {intf.reset_counters()},
-        ["switchport", "mode", "access"] => {intf.send_cmd(IntfCmd::PortModeAccess)},
-        ["switchport", "mode", "monitor", src_intf_str] => {
-            if intfs_view.contains_key(*src_intf_str) {
-              intf.send_cmd(IntfCmd::PortModeMonitoring(src_intf_str.to_string()));
-            } else {
-              println!("Interface {} not found", src_intf_str);
-            }
-        }
-        ["switchport", "access", "vlan", vlan_str] => {
-          match vlan_str.parse::<u16>() {
-            Ok(vlan) => {
-              if vlan > 0 && vlan < 4096 {
-                intf.send_cmd(IntfCmd::PortAccessVlan(vlan));
-                config.entry(mode.clone()).or_insert(HashSet::new()).insert(cmd.clone());
-              } else {
-                eprintln!("Error: invalid vlan \"{}\". Must be an number between 1 and 4095", vlan_str);
-              }
-            },
-            Err(err) => eprintln!("Error: invalid vlan format \"{}\". Must be number between 1 and 4095", vlan_str),
-          }
-        },
-        ["no", "switchport", "access", "vlan"] => {intf.send_cmd(IntfCmd::PortModeAccess)},
-        ["help"] => commands::display_help_menu(&mode),
-        ["exit"] => *mode = CliMode::General,
-        _ => println!("Unknown command"),
-      }
-    },
-    Err(ReadlineError::Interrupted) => println!("^C"),
-    Err(ReadlineError::Eof) => *mode = CliMode::General,
-    Err(_) => println!("No input"),
   }
 }
 
